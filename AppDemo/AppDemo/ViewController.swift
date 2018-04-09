bd9d52a0eaab0a4ac35cd1884d97a937bb3eb3bd
heguican <guican.he@brother-bsh.com.cn>
2018-04-09 14:55:35 +0800

    创建HelpIndex

--- AppDemo/AppDemo/ViewController.swift
+++ AppDemo/AppDemo/ViewController.swift
@@ -22,6 +22,45 @@ class ViewController: NSViewController {
         }
     }
 
+    @IBAction func createHelpIndex(_ sender: Any) {
+        guard let scriptPath = Bundle.main.path(forResource: "CreateHelpIndex", ofType: "scpt") else { return }
+        guard let infoPlistPath = Bundle.main.path(forResource: "Info", ofType: "plist") else { return }
+        guard let infoPlistStringPath = Bundle.main.path(forResource: "InfoPlist", ofType: "strings") else { return }
+        guard let HtmlPath = Bundle.main.path(forResource: "HelpIndexDemo", ofType: "html") else { return }
+        guard let deskTopPath = NSSearchPathForDirectoriesInDomains(.desktopDirectory, .userDomainMask, true).first else { return }
+        let helpInfoPlistPath = deskTopPath + "/HelpIndexDemo/Contents/Resources/Info.plist"
+        let helpInfoPlistStringPath = deskTopPath + "/HelpIndexDemo/Contents/Resources/zh-Hans.lproj/InfoPlist.strings"
+        let helpFolderPath = deskTopPath + "/HelpIndexDemo/Contents/Resources/zh-Hans.lproj/HelpIndexDemo"
+        let helpIndexPath = helpFolderPath + "/HelpIndexDemo.helpIndex"
+        let helpHtmlPath = helpFolderPath + "/HelpIndexDemo.html"
+        
+        do {
+            try FileManager.default.createDirectory(atPath: helpFolderPath, withIntermediateDirectories: true, attributes: nil)
+            try FileManager.default.copyItem(at: URL.init(fileURLWithPath: infoPlistPath), to: URL.init(fileURLWithPath: helpInfoPlistPath))
+            try FileManager.default.copyItem(at: URL.init(fileURLWithPath: infoPlistStringPath), to: URL.init(fileURLWithPath: helpInfoPlistStringPath))
+            try FileManager.default.copyItem(at: URL.init(fileURLWithPath: HtmlPath), to: URL.init(fileURLWithPath: helpHtmlPath))
+        } catch {
+            
+        }
+        
+        let createHelpIndexScript = "hiutil -C -a -m=3 -x \(helpInfoPlistPath) -f \(helpIndexPath) \(helpFolderPath)"
+//        let createHelpIndexScript = "hiutil -C -a -s=en -m=3 -x \(helpInfoPlistPath) -f \(helpIndexPath) \(helpFolderPath)"
+        let task = Process.launchedProcess(launchPath: "/usr/bin/osascript", arguments: [scriptPath, createHelpIndexScript])
+        task.waitUntilExit()
+        
+        let contentPath = deskTopPath + "/HelpIndexDemo"
+        let exportPath = deskTopPath + "/HelpIndexDemo.help"
+        do {
+            try FileManager.default.copyItem(at: URL.init(fileURLWithPath: contentPath), to: URL.init(fileURLWithPath: exportPath))
+            try FileManager.default.removeItem(at: URL.init(fileURLWithPath: contentPath))
+        } catch {
+            
+        }
+    }
+    
+    
+    
+    
 
 }
 
