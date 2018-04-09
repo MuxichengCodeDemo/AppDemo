158f27e45d888a6d4f175716549ce5311eb83969
heguican <guican.he@brother-bsh.com.cn>
2018-04-09 13:46:12 +0800

    初版

---
+++ AppDemo/AppDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AppDemo
+//
+//  Created by heguican on 2018/4/9.
+//  Copyright © 2018年 heguican. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
